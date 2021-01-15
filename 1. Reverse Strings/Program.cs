18a13e3716086b1edb198faeca566a0e549a7d2c
Petar Petrov <petar.dragov.petrov@gmail.com>
2021-01-15 10:55:48 +0100

    Add project files.

---
+++ 1. Reverse Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<string>
+        }
+    }
+}
